c89fda7bdf8046359a87129d38473ac16f530858
Maxfield-Dodge <113709023+Maxfield-Dodge@users.noreply.github.com>
2022-09-19 09:01:23 -0700

    Add files via upload
    Updated version

--- Madlib.java
+++ Madlib.java
@@ -46,8 +46,20 @@ public class Madlib
         String noun4 = input.nextLine();
         
         //prints result
-        System.out.println ("Our school cafeteria has really " + adj1 + "food. Just");
-        System.out.println ("thinking about it make my stomach " + verb1 + ". The");
-
+        System.out.println();
+        System.out.println(name);
+        System.out.println(month + "/" + day + "/" + year);
+        System.out.println();
+        System.out.println ("Our school cafeteria has really " + adj1 + " food. Just");
+        System.out.println ("thinking about it makes my stomach " + verb1 + ". The");
+        System.out.println("spaghetti is " + adj2 + " and tastes like " + noun1 + ". One");
+        System.out.println("day, I swear one of my meatballs started to " + verb2 + "! The");
+        System.out.println("turkey tacos are totally " + adj3 + " and look kind of like old");
+        System.out.println(noun2 + ". My friend Dana actually likes the meatloaf, even");
+        System.out.println("though it's " + adj4 + " and " + adj5 + ". I call it 'mystery");
+        System.out.println("meatloaf' and think it's really made out of " + noun3 + ". My");
+        System.out.println("dad said he'd make my lunches, but the first day, he made me");
+        System.out.println("a sandwich out of " + noun4 + " and peanut butter! I think I'd");
+        System.out.println("rather take my chances with the cafeteria!");
     }
 }
